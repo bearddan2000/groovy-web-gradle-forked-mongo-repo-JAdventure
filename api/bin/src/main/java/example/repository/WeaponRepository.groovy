a132b57309e15ab3eee2fb624b84acc1e7ee2ac4
bearddan2000 <67988550+bearddan2000@users.noreply.github.com>
2022-01-18 18:36:19 -0500

    Delete api/bin/src/main/java directory

--- api/bin/src/main/java/example/repository/WeaponRepository.groovy
+++
@@ -1,8 +0,0 @@
-package example.repository;
-
-import example.model.Weapon;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-// No need implementation, just one interface, and you have CRUD, thanks Spring Data!
-// MongoRepository’s methods: save(), findOne(), findById(), findAll(), count(), delete(), deleteById()
-public interface WeaponRepository extends MongoRepository<Weapon, String> {}
